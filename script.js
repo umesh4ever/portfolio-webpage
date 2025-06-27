3fe23624c11e4e1e8421a0f54aa9c68225bde4ee
mesh <154255289+umesh4ever@users.noreply.github.com>
2025-06-27 12:18:00 +0530

    tablet responsiveness

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+let hamBtn = document.getElementById("ham-btn");
+let mobileNavbar = document.getElementsByClassName("mobile-menu")[0];
+
+hamBtn.addEventListener("click", () => {
+
+   if(mobileNavbar.style.display === "none"){
+        mobileNavbar.style.display === "block"
+   }else{
+    mobileNavbar.style.display === "none"
+   }
+});
+
+hamBtn.addEventListener("click", () => {
+  mobileNavbar.classList.toggle("active");
+});
