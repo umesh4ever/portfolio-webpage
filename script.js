d1f482417a6f5806417d98bc2d06cb6bf4d7c504
mesh <154255289+umesh4ever@users.noreply.github.com>
2025-06-27 15:45:00 +0530

    update

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ document.getElementById("year").textContent = new Date().getFullYear();
 
 let hamBtn = document.getElementById("ham-btn");
 let mobileNavbar = document.getElementsByClassName("mobile-menu")[0];
-
-hamBtn.addEventListener("click", () => {
-
-   if(mobileNavbar.style.display === "none"){
-        mobileNavbar.style.display === "block"
-   }else{
-    mobileNavbar.style.display === "none"
-   }
-});
+let hamIcon = document.getElementsByClassName("fa-solid")[0];
 
 hamBtn.addEventListener("click", () => {
   mobileNavbar.classList.toggle("active");
+
+
+   if(hamIcon.classList.contains("fa-bars")){
+    
+        hamIcon.classList.remove("fa-bars");
+        hamIcon.classList.add("fa-xmark");
+   }else{
+        hamIcon.classList.remove("fa-xmark");
+        hamIcon.classList.add("fa-bars");
+
+
+   }
+
+   
 });
